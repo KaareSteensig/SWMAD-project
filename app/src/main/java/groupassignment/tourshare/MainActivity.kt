55e9d0e7e1be43b1bf2f34185cac4c6b0048c0c5
KaareSteensig <74143012+KaareSteensig@users.noreply.github.com>
2023-05-06 16:10:39 +0200

    Map showing and zooming to current location

--- app/src/main/java/groupassignment/tourshare/MainActivity.kt
+++ app/src/main/java/groupassignment/tourshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package groupassignment.tourshare
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -12,19 +13,24 @@ import android.util.Log
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.appcompat.widget.ActionMenuView
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.content.ContextCompat
 import backend.RepositoryMenus
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.rememberCameraPositionState
 import groupassignment.tourshare.Camera.CameraActivity
 import groupassignment.tourshare.gps.Service
+import groupassignment.tourshare.gps.routing.Route
+import groupassignment.tourshare.gps.routing.TAG_ROUTE
+import kotlinx.coroutines.launch
 import java.util.*
 
 class MainActivity : ComponentActivity(), OnMapReadyCallback  {
@@ -48,15 +54,15 @@ class MainActivity : ComponentActivity(), OnMapReadyCallback  {
         openCameraButton.setOnClickListener{
             //val CameraView = Intent(this@MainActivity, CameraActivity::class.java)
             //startActivity(CameraView)
-            if((ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA ) == PackageManager.PERMISSION_GRANTED)
-                && checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            if((ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA ) == PackageManager.PERMISSION_GRANTED)
+                && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
             {
                 val CameraView = Intent(this@MainActivity, CameraActivity::class.java)
                 startActivity(CameraView)
             }
             else
             {
-                val permission = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 requestPermissions(permission, Camera_Permission_Code)
                 //ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), Camera_Permission_Code)
                 //ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 112)
@@ -65,10 +71,7 @@ class MainActivity : ComponentActivity(), OnMapReadyCallback  {
 
         }
 
-
-
         val menu: ActionMenuView = findViewById(R.id.Menu_View)
-
 
         val openMenuButton: ImageButton = findViewById(R.id.Menu_Button)
         openMenuButton.setOnClickListener{
@@ -77,37 +80,49 @@ class MainActivity : ComponentActivity(), OnMapReadyCallback  {
 
         mapview = findViewById(R.id.Map_View)
 
-        if((ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
-            && checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        if((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
+            && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
             locationService.setLocationOn()
         }
         else {
             val permission = arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
             )
             requestPermissions(permission, 2)
             locationService.setLocationOn()
-            if((ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
-                && checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             }
         }
         mapview.onCreate(savedInstanceState)
-        mapview.getMapAsync(this)
+
+        findViewById<ComposeView>(R.id.my_composable).setContent {
+            val scope = rememberCoroutineScope()
+            LaunchedEffect(locationService.locationOn.value) {
+                scope.launch {
+                    location = locationService.getCurrentLocation()
+                    mapview.getMapAsync(this@MainActivity)
+                }
+            }
+        }
 
         val playButton: ImageButton = findViewById(R.id.Play_Button)
         playButton.setOnClickListener{
-
+            findViewById<ComposeView>(R.id.my_composable).setContent {
+                Route(locationService = locationService)
+            }
         }
 
     }
     override fun onMapReady(googleMap: GoogleMap) {
         googleMap.addMarker(
             MarkerOptions()
-                .position(LatLng(0.0,0.0))
+                .position(LatLng(location.latitude,location.longitude))
                 .title("Your position")
         )
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude,location.longitude),15f))
     }
     override fun onRequestPermissionsResult(
         requestCode: Int,
